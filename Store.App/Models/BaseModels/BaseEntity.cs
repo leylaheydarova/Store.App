ab0b9ed64927260cf1cdc75906f415386c1192ab
leylaheydarova <lbadalzade@gmail.com>
2024-08-09 18:45:28 +0400

    Project is started

---
+++ Store.App/Models/BaseModels/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.App.Models.BaseModels
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool isDeleted { get; set; }
+    }
+}
---
+++ Store.App/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿using Store.App.Models.BaseModels;
+
+namespace Store.App.Models
+{
+    public class Category:BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
